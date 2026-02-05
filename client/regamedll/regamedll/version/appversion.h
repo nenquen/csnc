5936143feb1873b393a18573eaf6a3d184cc446c
nenquen <barank2.3107@gmail.com>
2026-02-05 20:24:55 +0300

    cleaningggg

---
+++ client/regamedll/regamedll/version/appversion.h
@@ -0,0 +1,14 @@
+/* Generated app version header */
+#ifndef __APPVERSION_H__
+#define __APPVERSION_H__
+
+#define APP_VERSION "5.30.0.0-dev"
+#define APP_VERSION_C 5,30,0,0
+#define APP_VERSION_STRD "5.30.0.0"
+#define APP_VERSION_FLAGS 0x0L
+#define APP_COMMIT_DATE "Feb 5 2026"
+#define APP_COMMIT_TIME "00:00:00"
+#define APP_COMMIT_SHA "0000000"
+#define APP_COMMIT_URL ""
+
+#endif //__APPVERSION_H__
